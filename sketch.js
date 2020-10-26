05fb6218d6927fdb42bd93552198f88bc2cb23f9
Dhriti12 <71137659+Dhriti12@users.noreply.github.com>
2020-10-26 20:44:30 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+var ball,canvas;
+var database,position;
+var gameState = 0;
+var playerCount;
+var form,player,game;
+var allPlayers;
+var car1,car2,car3,car4,cars;
+
+function setup(){
+   
+    canvas = createCanvas(displayWidth-20,displayHeight-30);
+    database = firebase.database();
+    game = new Game();
+  game.getState();
+  game.start();
+}
+
+function draw(){
+  if(playerCount === 4){
+    game.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    game.play();
+  }
+   
+}
+
